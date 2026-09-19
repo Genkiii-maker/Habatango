30a45320ee2ae19e1164fea8508082dfe98bd47b
げんき <tengjingyongsi16@gmail.com>
2026-09-19 17:32:13 +0900

    Create words11.js

---
+++ words/words11.js
@@ -0,0 +1,11 @@
+const WORDS11 = [
+  {"no":1001,"word":"keep","star":false,"pos":"動","meaning":"保つ、持ち続ける","example":"Please keep the door closed.","circle":false},
+  {"no":1002,"word":"kind","star":true,"pos":"形","meaning":"親切な、種類","example":"She is very kind to everyone.","circle":false},
+  {"no":1003,"word":"king","star":false,"pos":"名","meaning":"王","example":"The king lived in a large castle.","circle":false},
+  {"no":1004,"word":"kitchen","star":true,"pos":"名","meaning":"台所、キッチン","example":"My mother is cooking in the kitchen.","circle":false},
+  {"no":1005,"word":"know","star":true,"pos":"動","meaning":"知っている、分かる","example":"I know his name.","circle":false},
+  {"no":1006,"word":"lake","star":false,"pos":"名","meaning":"湖","example":"There is a beautiful lake near here.","circle":false},
+  {"no":1007,"word":"language","star":true,"pos":"名","meaning":"言語、言葉","example":"English is an international language.","circle":false},
+  {"no":1008,"word":"large","star":true,"pos":"形","meaning":"大きい、広い","example":"They live in a large house.","circle":false},
+  {"no":1009,"word":"last","star":true,"pos":"形","meaning":"最後の、この前の、続く","example":"I saw him last Sunday.","circle":false},
+  {"no":1010,"word":"late","star":true
